a1a6c98ea563e31e79bb35c32000f2bd7bac1afc
TheEggi98 <55232322+TheEggi98@users.noreply.github.com>
2021-05-03 10:56:55 +0200

    Create fahrzeughersteller.php

---
+++ sites/fahrzeughersteller.php
@@ -0,0 +1,9 @@
+<body>
+  <?php
+      // require_once('../data/apis/fahrzeugherstellerApi.php');
+      include '../data/db.php';
+  
+      $conn = dbConnect();
+      // $fahrzeugApi = new FahrzeugherstellerApi.php;
+  ?>
+</body>
