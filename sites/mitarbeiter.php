243d0a475d1c45ea1c20778f7f00b420691c8963
TheEggi98 <55232322+TheEggi98@users.noreply.github.com>
2021-05-03 09:51:13 +0200

    Update mitarbeiter.php

--- sites/mitarbeiter.php
+++ sites/mitarbeiter.php
@@ -1,8 +1,11 @@
 <body>
   <?php
   require_once('../data/apis/mitarbeiterApi.php');
-  
+  include '../data/db.php';
   $mitarbeiterApi = new MitarbeiterApi();
+  $conn = dbConnect();
+  
+  print_r($mitarbeiterApi->getMitarbeiter($conn)
   ?>
   <h1>Mitarbeiter works!</h1>
 </body>
