4abf315b9121b466c751865a45a5e5a1371f3128
TheEggi98 <55232322+TheEggi98@users.noreply.github.com>
2021-05-03 10:53:00 +0200

    Create fahrzeug.php

---
+++ sites/fahrzeug.php
@@ -0,0 +1,9 @@
+<body>
+  <?php
+      // require_once('../data/apis/fahrzeugApi.php');
+      include '../data/db-php';
+  
+      $conn = dbConnect();
+      // $fahrzeugApi = new FahrzeugApi.php;
+  ?>
+</body>
