8fb20630bdc27f0787c607c5bab67f64ffb41cff
TheEggi98 <55232322+TheEggi98@users.noreply.github.com>
2021-05-03 10:54:20 +0200

    Create versicherungsgesellschaft.php

---
+++ sites/versicherungsgesellschaft.php
@@ -0,0 +1,9 @@
+<body>
+  <?php
+      // require_once('../data/apis/versicherungsgesellschaftApi.php');
+      include '../data/db.php';
+  
+      $conn = dbConnect();
+      // $fahrzeugApi = new VersicherungsgesellschaftApi.php;
+  ?>
+</body>
