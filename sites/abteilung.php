a6a3d5bffa398cb12c69bc6a795c0f6a5906b212
TheEggi98 <55232322+TheEggi98@users.noreply.github.com>
2021-05-03 10:56:28 +0200

    Create abteilung.php

---
+++ sites/abteilung.php
@@ -0,0 +1,9 @@
+<body>
+  <?php
+      // require_once('../data/apis/abteilungApi.php');
+      include '../data/db.php';
+  
+      $conn = dbConnect();
+      // $fahrzeugApi = new AbteilungApi.php;
+  ?>
+</body>
