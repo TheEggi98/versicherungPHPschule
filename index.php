67131ce60551859bee1b14292f63315723d49583
TheEggi98 <55232322+TheEggi98@users.noreply.github.com>
2021-05-03 11:40:57 +0200

    Update index.php

--- index.php
+++ index.php
@@ -9,18 +9,8 @@
   <title>Versicherung</title>
   </head>
   <body>  
-      <div>
-          <a href="sites/abteilung">Abteilungen</a>
-          <a href="sites/dienstwagen.php">Dienstwagen</a>
-          <a href="sites/fahrzeug.php">Fahrzeuge</a>
-          <a href="sites/fahrzeughersteller.php">Fahrzeughersteller</a>
-          <a href="sites/fahrzeugtyp.php">Fahrzeugtypen</a>
-          <a href="sites/mitarbeiter.php">Mitarbeiter</a>
-          <a href="sites/schadensfall.php">Schadensfall</a>
-          <a href="sites/tankvorgaenge.php">Tankvorgänge</a>
-          <a href="sites/versicherungsgesellschaft.php">Versicherungsgesellschaften</a>
-          <a href="sites/versicherungsnehmer.php">Versicherungsnehmer</a>
-          <a href="sites/vertrag.php">Verträge</a>
-      </div>
+ <?php
+    require("data/menu.php");
+ ?>
   </body>
 </html>
