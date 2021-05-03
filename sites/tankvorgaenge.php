516f7e6fc7188601a27d57ffcd3ab4928aee0332
TheEggi98 <55232322+TheEggi98@users.noreply.github.com>
2021-05-03 10:57:42 +0200

    Create tankvorgaenge.php

---
+++ sites/tankvorgaenge.php
@@ -0,0 +1,9 @@
+<body>
+  <?php
+      // require_once('../data/apis/tankvorgaengeApi.php');
+      include '../data/db.php';
+  
+      $conn = dbConnect();
+      // $tankvorgaengeApi = new TankvorgaengeApi.php;
+  ?>
+</body>
