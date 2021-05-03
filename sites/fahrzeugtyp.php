9912f388c36cdaca2b5275e87f698e89535dc003
TheEggi98 <55232322+TheEggi98@users.noreply.github.com>
2021-05-03 10:55:56 +0200

    Create fahrzeugtyp.php

---
+++ sites/fahrzeugtyp.php
@@ -0,0 +1,9 @@
+<body>
+  <?php
+      // require_once('../data/apis/fahrzeugtypApi.php');
+      include '../data/db.php';
+  
+      $conn = dbConnect();
+      // $fahrzeugApi = new FahrzeugtypApi.php;
+  ?>
+</body>
