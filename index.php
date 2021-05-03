4a6640ddbec5932b53e10eee55f162271653ba3a
TheEggi98 <55232322+TheEggi98@users.noreply.github.com>
2021-05-03 09:53:25 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,25 +1,17 @@
-<?php
-  include 'data/db.php';
-
-
-  $conn = dbConnect();
-?>
-
 <!DOCTYPE HTML>  
 <html>
-<?php // header('Content-type: html; charset=utf-8'); Macht Ä nur kaputt  ?>
-<head>
-<style>
-<?php include './style.css'; ?>
-</style>
+  <?php // header('Content-type: html; charset=utf-8'); Macht Ä nur kaputt  ?>
+  <head>
+  <style>
+  <?php include './style.css'; ?>
+  </style>
 
-<title>Versicherung</title>
-</head>
-<body>  
-    <div>
-        <a href="sites/mitarbeiter">Mitarbeiter</a>
-    </div>
-</body>
-<?php
-    dbCloseConnection($conn);
-?>
+  <title>Versicherung</title>
+  </head>
+  <body>  
+      <div>
+          <a href="sites/mitarbeiter">Mitarbeiter</a>
+          <a href="sites/vertrag.php">Verträge</a>
+      </div>
+  </body>
+</html>
