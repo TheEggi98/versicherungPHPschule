08208b4275fc29d69a33c27cfcd4571bcbbcc556
TheEggi98 <55232322+TheEggi98@users.noreply.github.com>
2021-05-03 10:53:32 +0200

    Create dienstwagen.php

---
+++ sites/dienstwagen.php
@@ -0,0 +1,9 @@
+<body>
+  <?php
+      // require_once('../data/apis/dienstwagenApi.php');
+      include '../data/db-php';
+  
+      $conn = dbConnect();
+      // $fahrzeugApi = new DienstwagenApi.php;
+  ?>
+</body>
