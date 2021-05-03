9c72d9e5afca276791dde43545c8c0db2337702a
TheEggi98 <55232322+TheEggi98@users.noreply.github.com>
2021-05-03 10:54:53 +0200

    Update dienstwagen.php

--- sites/dienstwagen.php
+++ sites/dienstwagen.php
@@ -1,7 +1,7 @@
 <body>
   <?php
       // require_once('../data/apis/dienstwagenApi.php');
-      include '../data/db-php';
+      include '../data/db.php';
   
       $conn = dbConnect();
       // $fahrzeugApi = new DienstwagenApi.php;
