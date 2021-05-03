c85c59f307940d8b9dde88ebce4edce9e4e8ed2c
TheEggi98 <55232322+TheEggi98@users.noreply.github.com>
2021-05-03 09:52:19 +0200

    Update vertrag.php

--- sites/vertrag.php
+++ sites/vertrag.php
@@ -1,6 +1,9 @@
 <body>
   <?php
   require_once('../data/apis/versicherungsvertragApi.php');
+  include '../data/db.php';
+  
+  $conn = dbConnect();
   
   $vertragsApi = new VersicherungsvertragApi();
   ?>
