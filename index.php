25f51969586d961dcb211cc27da844730cdd1320
TheEggi98 <55232322+TheEggi98@users.noreply.github.com>
2021-05-03 11:23:21 +0200

    Update index.php

--- index.php
+++ index.php
@@ -10,7 +10,16 @@
   </head>
   <body>  
       <div>
-          <a href="sites/mitarbeiter">Mitarbeiter</a>
+          <a href="sites/abteilung">Abteilungen</a>
+          <a href="sites/dienstwagen.php">Dienstwagen</a>
+          <a href="sites/fahrzeug.php">Fahrzeuge</a>
+          <a href="sites/fahrzeughersteller.php">Fahrzeughersteller</a>
+          <a href="sites/fahrzeugtyp.php">Fahrzeugtypen</a>
+          <a href="sites/mitarbeiter.php">Mitarbeiter</a>
+          <a href="sites/schadensfall.php">Schadensfall</a>
+          <a href="sites/tankvorgaenge.php">Tankvorgänge</a>
+          <a href="sites/versicherungsgesellschaft.php">Versicherungsgesellschaften</a>
+          <a href="sites/versicherungsnehmer.php">Versicherungsnehmer</a>
           <a href="sites/vertrag.php">Verträge</a>
       </div>
   </body>
