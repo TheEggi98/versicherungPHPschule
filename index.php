7ccf3a5d026b7fe610a9fb400cd0a96ed13760c2
TheEggi98 <55232322+TheEggi98@users.noreply.github.com>
2021-06-10 13:49:25 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <!DOCTYPE HTML>  
 <html>
-  <?php // header('Content-type: html; charset=utf-8'); Macht Ä nur kaputt  ?>
+  <?php header('Content-type: html; charset=utf-8'); ?>
   <head>
   <style>
   <?php include './style.css'; ?>
@@ -9,8 +9,10 @@
   <title>Versicherung</title>
   </head>
   <body>  
+  <div>
  <?php
     include __DIR__ . "/sites/menu.php";
  ?>
+ </div>
   </body>
 </html>
